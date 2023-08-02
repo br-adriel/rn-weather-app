f23df7af2a911f4f63a32d0b623d5315194f2f30
Adriel <adriel.fsantos@outlook.com>
2023-08-02 16:45:28 -0300

    Estrutura página de tempo

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 8</Text>
+          <Text style={styles.highLow}>Low: 6</Text>
+        </View>
+      </View>
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>It's sunny</Text>
+        <Text style={styles.message}>It's a perfect Tshirt weather</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  description: {
+    fontSize: 48,
+  },
+  feels: {
+    fontSize: 30,
+  },
+  highLow: {
+    fontSize: 20,
+  },
+  highLowWrapper: {
+    flexDirection: 'row',
+  },
+  message: {
+    fontSize: 30,
+  },
+  temp: {
+    fontSize: 48,
+  },
+  wrapper: {
+    flex: 1,
+    backgroundColor: 'skyblue',
   },
 });
