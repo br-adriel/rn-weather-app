a385314fc35d3a57b9f6595c555f416bca6282af
Adriel <adriel.fsantos@outlook.com>
2023-08-03 16:56:31 -0300

    Adiciona componente Badge

---
+++ src/components/Badge.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+function Badge({ iconName, text }) {
+  return (
+    <View style={styles.badgeWrapper}>
+      <Feather name={iconName} size={20} />
+      <Text>{text}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  badgeWrapper: {
+    backgroundColor: '#FFFA',
+    padding: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 8,
+    justifyContent: 'center',
+    gap: 4,
+  },
+});
+
+export default Badge;
