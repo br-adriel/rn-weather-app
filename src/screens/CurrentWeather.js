a18d6d7146ebf69950e3915c50a79aa6ffd197ce
Adriel <adriel.fsantos@outlook.com>
2023-08-03 18:39:40 -0300

    Estiliza página CurrentWeather

--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -1,59 +1,75 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { Feather } from '@expo/vector-icons';
+import {
+  ImageBackground,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import Temperature from '../components/Temperature';
+import Card from '../components/Card';
 
 function CurrentWeather() {
   return (
     <SafeAreaView style={styles.wrapper}>
-      <View style={styles.container}>
-        <Feather name='sun' size={100} />
-        <Text style={styles.temp}>6</Text>
-        <Text style={styles.feels}>Feels like 5</Text>
-        <View style={styles.highLowWrapper}>
-          <Text style={styles.highLow}>High: 8</Text>
-          <Text style={styles.highLow}>Low: 6</Text>
+      <StatusBar backgroundColor='transparent' translucent />
+      <ImageBackground
+        source={require('../../assets/img/bg_clouds.jpg')}
+        style={styles.image}
+      >
+        <View style={styles.container}>
+          <Temperature feels={5} temeperature={6} unit='C' />
+          <View style={styles.highLowWrapper}>
+            <Card style={styles.highLow}>
+              <Text style={styles.highLowtext}>High: 8</Text>
+            </Card>
+            <Card style={styles.highLow}>
+              <Text style={styles.highLowtext}>Low: 6</Text>
+            </Card>
+          </View>
+          <Card style={{ padding: 16 }}>
+            <Text style={styles.description}>It's sunny</Text>
+            <Text style={styles.message}>It's a perfect Tshirt weather</Text>
+          </Card>
         </View>
-      </View>
-      <View style={styles.bodyWrapper}>
-        <Text style={styles.description}>It's sunny</Text>
-        <Text style={styles.message}>It's a perfect Tshirt weather</Text>
-      </View>
+      </ImageBackground>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  bodyWrapper: {
-    justifyContent: 'flex-end',
-    alignItems: 'flex-start',
-    paddingLeft: 25,
-    paddingBottom: 40,
-  },
   container: {
+    paddingHorizontal: 16,
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    gap: 8,
+    paddingTop: StatusBar.currentHeight || 0,
   },
   description: {
-    fontSize: 48,
-  },
-  feels: {
-    fontSize: 30,
+    fontSize: 32,
   },
   highLow: {
+    padding: 16,
+    flexGrow: 1,
+  },
+  highLowtext: {
     fontSize: 20,
+    textAlign: 'center',
   },
   highLowWrapper: {
     flexDirection: 'row',
+    justifyContent: 'space-between',
+    gap: 8,
+  },
+  image: {
+    flex: 1,
+    objectFit: 'cover',
   },
   message: {
-    fontSize: 30,
-  },
-  temp: {
-    fontSize: 48,
+    fontSize: 18,
   },
   wrapper: {
     flex: 1,
-    backgroundColor: 'skyblue',
   },
 });
 
