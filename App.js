bb611c0df09425037ebea28c83ca755e3eef7cdb
Adriel <adriel.fsantos@outlook.com>
2023-08-04 23:39:55 -0300

    Implementa recuperação de dados a partir de API

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import { NavigationContainer } from '@react-navigation/native';
+import * as Location from 'expo-location';
 import { useEffect, useState } from 'react';
 import Tabs from './src/components/Tabs';
 import Loading from './src/screens/Loading';
-import * as Location from 'expo-location';
 
 export default function App() {
   const [loading, setLoading] = useState(true);
-  const [location, setLocation] = useState();
   const [error, setError] = useState();
+  const [weather, setWeather] = useState();
+  const [lat, setLat] = useState();
+  const [lon, setLon] = useState();
+
+  const fetchWeatherData = async () => {
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${process.env.EXPO_PUBLIC_WEATHER_API_KEY}`
+      );
+      const data = await res.json();
+      setWeather(data);
+    } catch (error) {
+      setError('could not fetch weather');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     (async () => {
@@ -17,8 +33,9 @@ export default function App() {
         return;
       }
       let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-      setLoading(false);
+      setLat(location.coords.latitude);
+      setLon(location.coords.longitude);
+      fetchWeatherData();
     })();
   }, []);
 
