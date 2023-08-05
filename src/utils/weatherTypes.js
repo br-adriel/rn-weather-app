cc899c6d7fbc65610ce77087ca25cab82394e030
Adriel <adriel.fsantos@outlook.com>
2023-08-04 22:56:44 -0300

    Adiciona objeto weatherTypes

---
+++ src/utils/weatherTypes.js
@@ -0,0 +1,42 @@
+export const weatherTypes = {
+  Clear: {
+    icon: 'sun',
+    message: "It's a hot day",
+    backgroundColor: '#00a8ff',
+  },
+  Clouds: {
+    icon: 'cloud',
+    message: 'You could live in the clouds',
+    backgroundColor: '#7ed6df',
+  },
+  Drizzle: {
+    icon: 'cloud-rain',
+    message: 'It might rain a little',
+    backgroundColor: '#487eb0',
+  },
+  Haze: {
+    icon: 'wind',
+    message: 'It might be a bit damp',
+    backgroundColor: '#95afc0',
+  },
+  Mist: {
+    icon: 'align-justify',
+    message: 'It might be hard to see',
+    backgroundColor: '#8395a7',
+  },
+  Rain: {
+    icon: 'umbrella',
+    message: "You'll need an umbrella",
+    backgroundColor: '#7f8fa6',
+  },
+  Snow: {
+    icon: 'cloud-snow',
+    message: "Let's build a snowman",
+    backgroundColor: '#9c88ff',
+  },
+  Thnderstorm: {
+    icon: 'zap',
+    message: 'It could get noisy',
+    backgroundColor: '#273c75',
+  },
+};
