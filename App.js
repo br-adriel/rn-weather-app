3b92e97610e1fbeed470c9c1a90dda3a019e108d
Adriel <adriel.fsantos@outlook.com>
2023-08-04 23:18:41 -0300

    Implementa obtenção de localização

--- App.js
+++ App.js
@@ -1,7 +1,28 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
 import Tabs from './src/components/Tabs';
+import Loading from './src/screens/Loading';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [location, setLocation] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setError('permission to access location was denied');
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      setLoading(false);
+    })();
+  }, []);
+
+  if (loading) return <Loading />;
   return (
     <NavigationContainer>
       <Tabs />
