3c5724b66c7a6509c28085562a879e4fe31a1f45
Adriel <adriel.fsantos@outlook.com>
2023-08-05 19:33:50 -0300

    Capitaliza primeira letra do título

--- src/components/MessageCard.js
+++ src/components/MessageCard.js
@@ -2,6 +2,10 @@ import { StyleSheet, Text } from 'react-native';
 import Card from './Card';
 
 function MessageCard({ title, message }) {
+  if (title && typeof title === 'string') {
+    title = title.charAt(0).toUpperCase() + title.slice(1);
+  }
+
   return (
     <Card style={{ padding: 16 }}>
       <Text style={styles.title}>{title}</Text>
