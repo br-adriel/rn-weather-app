eb07f2a9fde90b6b16ac0da732eae92c1f3631a8
Adriel <adriel.fsantos@outlook.com>
2023-08-06 18:14:36 -0300

    Reestiliza ListItem

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,33 +1,40 @@
 import { Feather } from '@expo/vector-icons';
 import { StyleSheet, Text, View } from 'react-native';
+import { weatherTypes } from '../utils/weatherTypes';
+import dayjs from 'dayjs';
 
 function ListItem({ dt_txt, min, max, condition }) {
   return (
     <View style={styles.item}>
-      <Feather name='sun' size={50} />
-      <Text style={styles.date}>{dt_txt}</Text>
-      <Text style={styles.temp}>{min}</Text>
-      <Text style={styles.temp}>{max}</Text>
+      <Text style={styles.date}>{dayjs(dt_txt).format('HH:mm')}</Text>
+      <Feather name={weatherTypes[condition].icon} size={64} />
+      <View style={styles.tempWrapper}>
+        <Feather name='thermometer' size={16} />
+        <Text style={styles.temp}>
+          {min.toFixed(1)}/{max.toFixed(1)}°C
+        </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   date: {
-    fontSize: 15,
+    fontSize: 14,
   },
   item: {
-    borderRadius: 8,
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
     alignItems: 'center',
     backgroundColor: '#FFFC',
+    borderRadius: 8,
+    gap: 4,
+    padding: 20,
   },
   temp: {
-    fontSize: 20,
+    fontSize: 18,
+  },
+  tempWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
 
