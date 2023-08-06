90f335c3af30562e353bd24fdbf13d148e5969ee
Adriel <adriel.fsantos@outlook.com>
2023-08-05 21:54:16 -0300

    Renderiza previsão dos dias seguintes a partir da API

--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -8,54 +8,16 @@ import {
 } from 'react-native';
 import ListItem from '../components/ListItem';
 
-const DATA = [
-  {
-    dt_txt: '2023-02-18 12:00:00',
-    main: {
-      temp_max: 8.55,
-      temp_min: 7.55,
-    },
-    weather: [
-      {
-        main: 'Clear',
-      },
-    ],
-  },
-  {
-    dt_txt: '2023-02-18 15:00:00',
-    main: {
-      temp_max: 8.55,
-      temp_min: 7.55,
-    },
-    weather: [
-      {
-        main: 'Clouds',
-      },
-    ],
-  },
-  {
-    dt_txt: '2023-02-18 18:00:00',
-    main: {
-      temp_max: 8.55,
-      temp_min: 7.55,
-    },
-    weather: [
-      {
-        main: 'Rain',
-      },
-    ],
-  },
-];
-
-function UpcomingWeather() {
+function UpcomingWeather({ weatherData }) {
   const renderItem = ({ item }) => (
     <ListItem
       condition={item.weather[0].main}
       dt_txt={item.dt_txt}
-      max={item.main.temp_max}
       min={item.main.temp_min}
+      max={item.main.temp_max}
     />
   );
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar translucent backgroundColor='transparent' />
@@ -65,7 +27,7 @@ function UpcomingWeather() {
       >
         <Text style={styles.title}>Upcoming</Text>
         <FlatList
-          data={DATA}
+          data={weatherData}
           renderItem={renderItem}
           keyExtractor={(item) => item.dt_txt}
         />
