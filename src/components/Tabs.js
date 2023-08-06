e39db468ab917460b773456615e9e350afc5c917
Adriel <adriel.fsantos@outlook.com>
2023-08-05 23:18:28 -0300

    Remove tela de cidade

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,8 +1,9 @@
 import { Feather } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import City from '../screens/City';
 import CurrentWeather from '../screens/CurrentWeather';
 import UpcomingWeather from '../screens/UpcomingWeather';
+import { BlurView } from 'expo-blur';
+import { StyleSheet } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,20 +13,36 @@ export default function Tabs({ weather }) {
       initialRouteName='Current'
       screenOptions={{
         headerShown: false,
-        tabBarActiveBackgroundColor: '#FFFD',
         tabBarActiveTintColor: '#000',
         tabBarLabelPosition: 'beside-icon',
+        tabBarBackground: () => (
+          <BlurView
+            style={StyleSheet.absoluteFill}
+            intensity={80}
+            tint='light'
+          />
+        ),
         tabBarStyle: {
-          backgroundColor: '#FFFC',
-          borderTopLeftRadius: 8,
-          borderTopRightRadius: 8,
-          left: '2%',
-          overflow: 'hidden',
+          backgroundColor: 'transparent',
+          elevation: 0,
           position: 'absolute',
-          width: '96%',
+          shadowOffset: {
+            height: 0,
+            width: 0,
+          },
         },
       }}
     >
+      <Tab.Screen
+        name='Current'
+        options={{
+          tabBarIcon: (props) => (
+            <Feather name='cloud' color={props.color} size={props.size} />
+          ),
+        }}
+      >
+        {() => <CurrentWeather weatherData={weather} />}
+      </Tab.Screen>
       <Tab.Screen
         name='Upcoming'
         options={{
@@ -35,26 +52,6 @@ export default function Tabs({ weather }) {
         }}
       >
         {() => <UpcomingWeather weatherData={weather.list} />}
-      </Tab.Screen>
-      <Tab.Screen
-        name='Current'
-        options={{
-          tabBarIcon: (props) => (
-            <Feather name='cloud' color={props.color} size={props.size} />
-          ),
-        }}
-      >
-        {() => <CurrentWeather weatherData={weather.list[0]} />}
-      </Tab.Screen>
-      <Tab.Screen
-        name='City'
-        options={{
-          tabBarIcon: (props) => (
-            <Feather name='map-pin' color={props.color} size={props.size} />
-          ),
-        }}
-      >
-        {() => <City weatherData={weather.city} />}
       </Tab.Screen>
     </Tab.Navigator>
   );
