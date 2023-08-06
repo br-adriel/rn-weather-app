de32d29d74b5f67af21cdeadbd90a31a522b109b
Adriel <adriel.fsantos@outlook.com>
2023-08-05 23:18:02 -0300

    Reestiliza página inicial

--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -1,42 +1,58 @@
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+import { LinearGradient } from 'expo-linear-gradient';
 import {
   ImageBackground,
   SafeAreaView,
+  ScrollView,
   StatusBar,
   StyleSheet,
   View,
 } from 'react-native';
 import HighLowTemperatures from '../components/HighLowTemperatures';
+import Location from '../components/Location';
 import MessageCard from '../components/MessageCard';
 import Temperature from '../components/Temperature';
 import { weatherTypes } from '../utils/weatherTypes';
 
 function CurrentWeather({ weatherData }) {
+  const padBottom = useBottomTabBarHeight();
+
   const {
     main: { temp, feels_like, temp_max, temp_min },
     weather,
-  } = weatherData;
+  } = weatherData.list[0];
+  const { name, country, sunrise, sunset } = weatherData.city;
   const weatherCondition = weather[0].main;
 
   return (
-    <SafeAreaView style={styles.wrapper}>
+    <SafeAreaView style={[styles.wrapper, { paddingBottom: padBottom }]}>
       <StatusBar backgroundColor='transparent' translucent />
       <ImageBackground
+        style={{ flex: 1 }}
         source={require('../../assets/img/bg_clouds.jpg')}
-        style={styles.image}
       >
-        <View style={styles.container}>
-          <Temperature
-            feels={feels_like}
-            temeperature={temp}
-            unit='C'
-            iconName={weatherTypes[weatherCondition].icon}
-          />
-          <HighLowTemperatures high={temp_max} low={temp_min} />
-          <MessageCard
-            title={weather[0].description}
-            message={weatherTypes[weatherCondition].message}
-          />
-        </View>
+        <LinearGradient
+          style={styles.wrapper}
+          colors={['transparent', '#4EA3DB', '#6BBFDB']}
+        >
+          <ScrollView>
+            <View style={styles.container}>
+              <View style={{ height: 200 }} />
+              <Location country={country} name={name} />
+              <Temperature
+                feels={feels_like}
+                temeperature={temp}
+                unit='C'
+                iconName={weatherTypes[weatherCondition].icon}
+              />
+              <HighLowTemperatures high={temp_max} low={temp_min} />
+              <MessageCard
+                title={weather[0].description}
+                message={weatherTypes[weatherCondition].message}
+              />
+            </View>
+          </ScrollView>
+        </LinearGradient>
       </ImageBackground>
     </SafeAreaView>
   );
@@ -46,13 +62,9 @@ const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 16,
     flex: 1,
-    justifyContent: 'center',
     gap: 8,
-    paddingTop: StatusBar.currentHeight || 0,
-  },
-  image: {
-    flex: 1,
-    objectFit: 'cover',
+    paddingTop: 8 + (StatusBar.currentHeight || 0),
+    paddingBottom: 16,
   },
   wrapper: {
     flex: 1,
